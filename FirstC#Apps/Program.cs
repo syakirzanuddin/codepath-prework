94e080745b9457010185881d74a4ed3d730f48ee
Muhd Syakir <syakirzanuddin98@gmail.com>
2023-11-22 21:35:38 +0800

    Added existing file to repository

---
+++ FirstC#Apps/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static int myVar;
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            Program b = new Program();
+            Console.WriteLine("Enter number: ");
+            a.Test(Convert.ToInt32(Console.ReadLine()));
+            myVar = 10;
+            myVar = 11;
+            Console.WriteLine("a: " + myVar);
+            Console.WriteLine("b: " + myVar);
+
+        }
+
+        public void Test(int i)
+        {
+            if (i < 9)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            else
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+    }
+}
